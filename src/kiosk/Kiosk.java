e8eb8dae1260b6f3afafa167b3e73dff8deebe0f
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-10 17:07:40 +0900

    fix(Kiosk) : fix infinity loop caused by exceptions

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -37,23 +37,25 @@ public class Kiosk {
     //main에서 관리하던 입력과 반복문 로직 관리
     public void start(){
         Scanner input = new Scanner(System.in);
-        int choice = 0;
+        int choice = -1;
         //반복문을 이용해 0 입력 시, 프로그램 종료
-        try {
-            do {
+        do {
+            try {
                 //출력
                 printList("SHAKESHACK MENU");
                 //입력 받기 & 개행 문자 제거
                 choice = input.nextInt();
                 input.nextLine();
-            } while (choice != 0);
-        } catch(InputMismatchException e){
-            System.out.println("잘못된 형식의 입력입니다.");
-        } catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("해당 값은 선택할 수 없습니다.");
-        } catch(Exception e){
-            System.out.println("알 수 없는 오류입니다." + e);
-        }
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 형식의 입력입니다.");
+                input.nextLine();
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("해당 값은 선택할 수 없습니다.");
+                input.nextLine();
+            } catch (Exception e) {
+                System.out.println("알 수 없는 오류입니다." + e);
+            }
+        } while (choice != 0);
         System.out.println("프로그램을 종료합니다.");
     }
 }
