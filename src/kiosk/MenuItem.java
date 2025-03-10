4320d5faadd165eb31f23a564f15f8781274969e
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-10 12:05:34 +0900

    feat : implement constructor, getter, setter and encapsulation

--- src/kiosk/MenuItem.java
+++ src/kiosk/MenuItem.java
@@ -1,4 +1,36 @@
 package kiosk;
 
+import javax.lang.model.type.NullType;
+
 public class MenuItem {
+    //캡슐화
+    private String name;
+    private Double price;
+    private String description;
+    //생성자
+    public MenuItem(String name, Double price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+    //getter
+    public String getName(){
+        return this.name;
+    }
+    public Double getPrice(){
+        return this.price;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    //setter
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPrice(Double price){
+        this.price = price;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
