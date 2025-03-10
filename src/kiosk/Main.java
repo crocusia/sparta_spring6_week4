f3a384596aba99ab6bc169ba70a9935e421b4c1e
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-10 15:08:08 +0900

    comment(Main) : add comment

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -5,36 +5,45 @@ import jdk.jshell.SourceCodeAnalysis;
 import java.util.Scanner;
 
 public class Main {
+    //제목 출력 메서드
     public static void printTitle(String title){
         System.out.println("[ " + title + " ]");
     }
+    //MenuItem 출력 메서드
     public static void printMenuItem(int index, MenuItem item){
         String name = item.getName();
         Double price = item.getPrice();
         String desc = item.getDescription();
         System.out.printf("%d. %-15s| W %.1f | %s%n", index, name, price, desc);
     }
+    //종료 여부 출력 메서드
     public static void printExit(){
         System.out.println("0. 종료");
     }
 
     public static void main(String[] args){
+        //Menu 객체 생성 및 MenuItem 객체 생성 후 리스트에 add
         Menu burgerMenu = new Menu("Burger");
         burgerMenu.addMenu(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         burgerMenu.addMenu(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         burgerMenu.addMenu(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         burgerMenu.addMenu(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
+        //Scanner로 숫자를 입력받기
         Scanner input = new Scanner(System.in);
         int choice = 0;
+        //반복문을 이용해 0 입력 시, 프로그램 종료
         do{
+            //메뉴 리스트 출력
             printTitle("SHAKESHACK MENU");
             for(int i = 1; i < burgerMenu.getListSize()+1; i++){
                 printMenuItem(i, burgerMenu.getMenuItem(i-1));
             }
             printExit();
 
+            //입력 받기
             choice = input.nextInt();
+            //개행 문자 제거
             input.nextLine();
         }while(choice != 0);
         System.out.println("프로그램을 종료합니다.");
