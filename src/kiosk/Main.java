ee7eab4e1a09351659ab8f56889a1a8a9c0d89a2
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-10 15:03:27 +0900

    feat(Main) : add do-while and complete lv1

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void printExit(){
         System.out.println("0. 종료");
     }
+
     public static void main(String[] args){
         Menu burgerMenu = new Menu("Burger");
         burgerMenu.addMenu(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
@@ -24,10 +25,18 @@ public class Main {
         burgerMenu.addMenu(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         burgerMenu.addMenu(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        printTitle("SHAKESHACK MENU");
-        for(int i = 1; i < burgerMenu.getListSize()+1; i++){
-            printMenuItem(i, burgerMenu.getMenuItem(i-1));
-        }
-        printExit();
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+        do{
+            printTitle("SHAKESHACK MENU");
+            for(int i = 1; i < burgerMenu.getListSize()+1; i++){
+                printMenuItem(i, burgerMenu.getMenuItem(i-1));
+            }
+            printExit();
+
+            choice = input.nextInt();
+            input.nextLine();
+        }while(choice != 0);
+        System.out.println("프로그램을 종료합니다.");
     }
 }
